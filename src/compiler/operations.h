4fea13fb4f066e3d9aaa5be1aa3cc358ffb14b69
Sasha <aloxwastaken@gmail.com>
2023-03-22 00:27:40 +0200

    Added op_to_str function

--- src/compiler/operations.h
+++ src/compiler/operations.h
@@ -1,6 +1,6 @@
 #pragma once
 
-enum operation {
+enum Operation {
     // LOADs
     OP_LOAD_CONST,      // direct: constant index, stack: none
     OP_LOAD_NAME,       // direct: name index, stack: none
@@ -17,7 +17,7 @@ enum operation {
     // FUNCTIONs
     OP_CALL,                // direct: argument count, stack: object, ...
     OP_CALL_METHOD,         // direct: argument count, stack: object, ..., self object
-    OP_FUNCTION_DEFNITION,  // direct: none, stack: object
+    OP_FUNCTION_DEFNITION,  // direct: none, stack: code object
     OP_RETURN,              // direct: none, stack: object
 
     // LOOPs
@@ -25,14 +25,14 @@ enum operation {
     OP_POP_JUMP_IF_FALSE,   // direct: relative byte offset, stack: object
 };
 
-enum unary_operator {
+enum UnaryOperator {
     UNARY_ADD,
     UNARY_SUB,
     UNARY_NOT,
     UNARY_BOOL_NOT,
 };
 
-enum binary_operator {
+enum BinaryOperator {
     BINARY_ADD,
     BINARY_SUB,
     BINARY_MUL,
@@ -51,3 +51,22 @@ enum binary_operator {
     BINARY_BOOLEAN_GREATER_THAN,
     BINARY_BOOLEAN_LESS_THAN,
 };
+
+char* op_to_str(enum Operation op) {
+    switch(op) {
+        case OP_LOAD_CONST:         return "LOAD_CONST";       
+        case OP_LOAD_NAME:          return "LOAD_NAME";        
+        case OP_LOAD_ATTRIBUTE:     return "LOAD_ATTRIBUTE";   
+        case OP_STORE_NAME:         return "STORE_NAME";       
+        case OP_STORE_ATTRIBUTE:    return "STORE_ATTRIBUTE";  
+        case OP_UNARY:              return "UNARY";            
+        case OP_BINARY:             return "BINARY";           
+        case OP_CALL:               return "CALL";               
+        case OP_CALL_METHOD:        return "CALL_METHOD";        
+        case OP_FUNCTION_DEFNITION: return "FUNCTION_DEFNITION"; 
+        case OP_RETURN:             return "RETURN";             
+        case OP_JUMP:               return "JUMP";               
+        case OP_POP_JUMP_IF_FALSE:  return "POP_JUMP_IF_FALSE";  
+    }
+    return "OP_UNKNOWN";
+}
