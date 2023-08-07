0225161cc4e73faec3b705c54715292a95fe3e0e
Sasha <aloxwastaken@gmail.com>
2023-08-07 23:59:40 +0300

    Changed defines to be compatible with windows.h

--- new/src/defines.h
+++ new/src/defines.h
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <limits.h>
+#include "da.h"
 
 // #define DEBUG_TOKEN
 // #define DEBUG_LEXER
@@ -24,19 +25,19 @@
 #define WARNING(msg) LOG("Warning", msg)
 #define WARNING_ARGS(fmt, ...) LOG_ARGS("Warning", fmt, __VA_ARGS__)
 
-#define ERROR(type, msg) printf("%sError: %s\n", type, msg)
+#define ERROR_LOG(type, msg) printf("%sError: %s\n", type, msg)
 #define ERROR_ARGS(type, fmt, ...) printf("%sError: " fmt "\n", type, __VA_ARGS__)
 
 #define UNIMPLEMENTED printf("Unimplemented: " __FILE__ ":%d inside %s\n", __LINE__, __FUNCTION__)
 
-#define unwrap(result) {Result res = result; if (res.is_err) { ERROR("Unwrap", res.what); exit(1); } }
+#define unwrap(result) {Result res = result; if (res.is_err) { ERROR_LOG("Unwrap", res.what); exit(1); } }
 
 #define isdigit(c) (c >= '0' && c <= '9')
 #define isspace(c) (c == ' ' || c == '\r' || c == '\n' || c == '\t')
 #define tolower(c) ((c >= 'A' && c <= 'Z') ? c | (1 << 5) : c)
 #define toupper(c) ((c >= 'a' && c <= 'z') ? c & ~(1 << 5) : c)
 
-#define ARRAYSIZE(arr) (sizeof(arr) / sizeof(arr[0]))
+#define STATIC_ARRAYSIZE(arr) (sizeof(arr) / sizeof(arr[0]))
 
 #define MIN(a, b) (a < b ? a : b)
 #define MAX(a, b) (a > b ? a : b)
